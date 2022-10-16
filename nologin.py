c9501a01230f00c41278378cf12789cc33374147
Qaiser Abbas <69212320+TechQaiser@users.noreply.github.com>
2022-10-16 21:51:03 +0500

    Update nologin.py

--- nologin.py
+++ nologin.py
@@ -1,2 +1,11 @@
-import nologin
-nologin.main()
+
+import sys,time
+from platform import uname
+from os import path,system
+arch=uname().machine.lower()
+if "aarch" in arch:
+	import nologin
+	nologin.main()
+else:
+	import nologin32
+	nologin32.main()
